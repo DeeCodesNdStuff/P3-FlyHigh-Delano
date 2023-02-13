680eacb242c46ffb7c16caee74faef1f5f0750c4
DeeCodesNdStuff <124685125+DeeCodesNdStuff@users.noreply.github.com>
2023-02-13 12:37:28 +0100

    added my own code
    just addes sum things

--- FlyHigh/Assets/Scripts!/Delano/DeeMovement.cs
+++ FlyHigh/Assets/Scripts!/Delano/DeeMovement.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Delano : MonoBehaviour
+public class DeeMovement : MonoBehaviour
 {
+    public float moveSpeed;
+    public float cameraSpeed;
+
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
