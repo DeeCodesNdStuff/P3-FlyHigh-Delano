9156fd28eef25f7d9b4e4978794c4a0020bec2d5
DeeCodesNdStuff <124685125+DeeCodesNdStuff@users.noreply.github.com>
2023-03-27 14:23:25 +0200

    ui part
    shotgun added ui for reloading, needs tweaks

---
+++ FlyHigh/Assets/Scripts!/Delano/Items/ItemScripts/Shootingusing/ShootingShotgun.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShootingShotgun : MonoBehaviour
+{
+    public Shotgun shootGuns;
+
+    public RaycastHit hit;
+
+    public TMP_Text textMeshpr0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            shootGuns.currentBullets -=1;
+            if (Physics.Raycast(transform.position, transform.forward, out hit, 1000))
+            {
+                if (hit.transform.gameObject.tag == "Enemy")
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            shootGuns.currentBullets = shootGuns.maxBullets;
+        }
+
+        if (shootGuns.currentBullets == 0)
+        {
+            textMeshpr0.text = shootGuns.text[0];
+        }
+        if (shootGuns.currentBullets > 0)
+        {
+            textMeshpr0.text = shootGuns.text[1];
+        }
+    }
+}
