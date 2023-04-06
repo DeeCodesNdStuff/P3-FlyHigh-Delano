dfe1ac5fef1a5590506e281523c439392a7fde97
Luukiep <97096896@st.deltion.nl>
2023-04-06 13:48:05 +0200

    Inproved Movement

--- FlyHigh/Assets/Scripts!/Luuk/CamMovement.cs
+++ FlyHigh/Assets/Scripts!/Luuk/CamMovement.cs
@@ -4,29 +4,26 @@ using UnityEngine;
 
 public class CamMovement : MonoBehaviour
 {
-    public float cy;
     public float speedH;
     public float speedV;
     private float rX;
     private float rY;
     public Transform tar;
     public float dis;
-    // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         speedH = 5f;
         speedV = 3f;
         dis= 3f;
     }
-
-    // Update is called once per frame
     void Update()
     {
         rX -= speedH * Input.GetAxis("Mouse Y");
         rY += speedV * Input.GetAxis("Mouse X");
         rX = Mathf.Clamp(rX, -10f, 30f);
         transform.eulerAngles = new Vector3(rX, rY, 0);
-        transform.position = tar.position - transform.forward * dis ;
+        transform.position = tar.position - transform.forward * dis;
     }
 }
--- FlyHigh/Assets/Scripts!/Luuk/Movement.cs
+++ FlyHigh/Assets/Scripts!/Luuk/Movement.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Movement : MonoBehaviour
 {
     public bool isGrounded;
+    public bool isRunning;
     public float speed;
-    public float speedV;
-    public int speedG;
+    public float speedCa;
+    public float speedGi;
     public float jumpF;
     public float hor;
     public float ver;
@@ -15,66 +18,90 @@ public class Movement : MonoBehaviour
     public Vector3 move;
     public Vector3 jump;
     public Rigidbody rb;
-    // Start is called before the first frame update
+    public Animator animator;
     void Start()
     {
         speed = 6f;
-        jumpF = 2.1f;
-        speedV = 3f;
+        speedCa = 3f;
+        speedGi = 5f;
+        jumpF = 2.3f;
         rb = GetComponent<Rigidbody>();
-        jump = new Vector3(0.0f, 2.1f, 0.0f);
-        isGrounded = true;
+        animator = GetComponent<Animator>();
+        jump = new Vector3(0.0f, 2.3f, 0.0f);
     }
     void OnCollisionStay()
     {
         isGrounded = true;
+        rb.drag = 0f;
     }
-
-    // Update is called once per frame
     void Update()
     {
+        //movement
         move.x = hor;
         move.z = ver;
         hor = Input.GetAxis("Horizontal");
         ver = Input.GetAxis("Vertical");
-        transform.Translate(move * Time.deltaTime * speed);
+        transform.Translate(move * speed * Time.deltaTime);
 
-        rY += speedV * Input.GetAxis("Mouse X");
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            animator.SetTrigger("IsWalking");
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetTrigger("IsStanding");
+        }
+
+        //dash
+        if (Input.GetKeyDown(KeyCode.LeftShift) && isGrounded)
+        {
+            isRunning = true;
+            speed = 8f;
+            animator.SetTrigger("IsRunning");
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isRunning = false;
+            speed = 6f;
+            animator.SetTrigger("IsBackWalking");
+        }
+
+        //camera
+        rY += speedCa * Input.GetAxis("Mouse X");
         transform.eulerAngles = new Vector3(0, rY, 0);
 
+        //jump
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(jump * jumpF, ForceMode.Impulse);
             isGrounded = false;
         }
 
-        if (isGrounded == true)
-        {
-            rb.drag = 0f;
-        }
-
-        if (Input.GetKeyDown(KeyCode.Q) && isGrounded == false)
+        //gliding
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == false)
         {
             rb.drag = 5f;
 
             if (Input.GetKeyDown(KeyCode.W))
             {
-                GetComponent<Rigidbody>().AddForce(new Vector3(speedG,0,0));
+               rb.AddForce(new Vector3(speedGi,0,0));
             }
 
             if (Input.GetKeyDown(KeyCode.A))
             {
-                GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, speedG));
+                rb.AddForce(new Vector3(0, 0, speedGi));
             }
 
             if (Input.GetKeyDown(KeyCode.S))
             {
-                GetComponent<Rigidbody>().AddForce(new Vector3(-speedG, 0, 0));
+                rb.AddForce(new Vector3(-speedGi, 0, 0));
             }
 
             if (Input.GetKeyDown(KeyCode.D))
             {
-                GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -speedG));
+                rb.AddForce(new Vector3(0, 0, -speedGi));
             }
         }   
     }
